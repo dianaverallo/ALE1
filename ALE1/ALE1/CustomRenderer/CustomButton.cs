718abe8ea77d9f098245d8c22e9f2e3497fa0a18
Diana Verallo <001649@lrmc.com.ph>
2018-01-10 16:57:16 +0800

    Add project files.

---
+++ ALE1/ALE1/CustomRenderer/CustomButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace ALE1.CustomRenderer
+{
+    public class CustomButton : Button
+    {
+
+    }
+}
